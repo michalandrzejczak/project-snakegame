623edf7618e19c198c0e8e800c8cd49e81c9d601
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-03-06 12:59:13 +0100

    Added collisions, food and score counter - ver. 1.0

--- game.js
+++ game.js
@@ -3,19 +3,26 @@ let cx = canvas.getContext("2d");
 
 let step = 25;
 
-let startSnakePosX = step, startSnakePosY = step, prevposX, prevposY;
+let startSnakePosX = step, startSnakePosY = step;
 let startSnakeLength = 6;
 let snakeBodyParts = [];
 let snakeLength = startSnakeLength;
 
+let foodX, foodY;
+
 let direction = "right";
+
+let score = 0;
 
 function initSnake() {
     for (let i=0; i<startSnakeLength; i++) {
         snakeBodyParts.push({x: i, y: 0});
     };
+    direction = "right";
+    score = 0;
 };
 initSnake();
+positionFood();
 
 function drawSnake() {
     cx.clearRect(0, 0, canvas.width, canvas.height);
@@ -24,10 +31,29 @@ function drawSnake() {
         cx.fillRect(snakeBodyParts[i].x*step, snakeBodyParts[i].y*step,25,25);
         cx.fillStyle = "#191921";
         cx.fillRect(snakeBodyParts[snakeBodyParts.length-1].x*step,
-                   snakeBodyParts[snakeBodyParts.length-1].y*step, 25,25);
+                   snakeBodyParts[snakeBodyParts.length-1].y*step, 25, 25);
     };
         
-    moveSnake();  
+    moveSnake();
+    checkCollision();
+    drawFood();
+    cx.font = "15px monospace";
+    cx.fillText("Score:"+score, 0, 495)
+};
+
+
+
+function positionFood() {
+    foodX = Math.floor(Math.random()*20);
+    foodY = Math.floor(Math.random()*20);
+};
+
+function drawFood() {
+    cx.fillStyle = "#191921";
+    cx.fillRect(foodX*step+8.5, foodY*step, 8, 8)
+    cx.fillRect(foodX*step+17, foodY*step+8.5, 8, 8)
+    cx.fillRect(foodX*step+8.5, foodY*step+17, 8, 8)
+    cx.fillRect(foodX*step, foodY*step+8.5, 8, 8)
 };
 
 
@@ -86,8 +112,8 @@ function animateSnake() {
     directSnake();
     
     setInterval(function() {
-        if (paused == false) 
-        drawSnake()
+        if (paused == false)
+           drawSnake();
         else;
     }, 150)
 };
@@ -96,9 +122,46 @@ animateSnake();
 
 function pause() {
     window.addEventListener("keydown", event =>  {
-        if (event.keyCode == 32 && paused == true) {paused = false;}
-        else if (event.keyCode == 32 && paused == false) {paused = true;};  
+        if (event.keyCode == 32 && paused == true) {
+            paused = false;
+        }
+        else if (event.keyCode == 32 && paused == false) {
+            paused = true; 
+            cx.font = "30px monospace";
+            cx.fillText("Pause",210,250)
+        };  
     });
 };
 pause();
+
+function gameOver() {
+    snakeBodyParts = [];
+    paused = true;
+    cx.clearRect(0, 0, canvas.width, canvas.height);
+    cx.font = "30px monospace";
+    cx.fillText("Game over", 180, 250);
+    cx.font = "20px monospace";
+    cx.fillText("Your score: "+score, 180, 290); 
+    initSnake();
+};
  
+function checkCollision() {
+let snakeHeadX = snakeBodyParts[snakeBodyParts.length-1].x;
+let snakeHeadY = snakeBodyParts[snakeBodyParts.length-1].y;
+    if (snakeHeadX<-1 ||
+        snakeHeadX>20 ||
+        snakeHeadY<-1 ||
+        snakeHeadY>20)
+        gameOver();
+    for (let i=0; i<snakeBodyParts.length-1; i++) {
+        if (snakeHeadX == snakeBodyParts[i].x && 
+            snakeHeadY == snakeBodyParts[i].y)
+            gameOver();
+    };
+    if (snakeHeadX == foodX && snakeHeadY == foodY) {
+        snakeLength++;
+        score+=100;
+        snakeBodyParts.unshift({x:-1, y:-1});
+        positionFood();
+    };
+};
